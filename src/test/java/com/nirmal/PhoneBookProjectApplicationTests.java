3078c443d071291640e5d98706d5e357a8b23713
nileshpatil45 <nileshp2626@gmail.com>
2022-09-03 15:02:36 +0530

    PBA01 Create gitHub repository

---
+++ src/test/java/com/nirmal/PhoneBookProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nirmal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneBookProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
