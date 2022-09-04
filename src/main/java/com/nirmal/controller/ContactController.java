f41833b83738ed4dfc4e1a7c79462942ceaf7386
nileshpatil45 <nileshp2626@gmail.com>
2022-09-04 10:30:45 +0530

    PBA04 get all contact details from database

--- src/main/java/com/nirmal/controller/ContactController.java
+++ src/main/java/com/nirmal/controller/ContactController.java
@@ -1,8 +1,11 @@
 package com.nirmal.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,15 +21,32 @@ public class ContactController {
 	
 	@PostMapping(value="/save",consumes="application/json")
 	public ResponseEntity <String> save(@RequestBody Contact contact){
+		
 		boolean savecontact = this.contactservicei.savecontact(contact);
 		if(savecontact==true) {
 			return new ResponseEntity<String>("contact successfully save",HttpStatus.OK);
 		}
 		else {
 			return new ResponseEntity<String>("contact not save",HttpStatus.BAD_REQUEST);
-
-
 		}
+		
+	}	
+		@GetMapping(value="/getall",produces="application/json")
+		public ResponseEntity <List<Contact>> getallcontact(){
+			
+			List<Contact> getall = this.contactservicei.getall();
+			if(getall !=null ) {
+				return new ResponseEntity<List<Contact>>(getall,HttpStatus.OK);
+			}
+			else {
+				String msg ="data not found";
+		return new ResponseEntity(msg,HttpStatus.BAD_REQUEST);
+			}
+			
+			
+		
+
+		
 		
 	}
 }
